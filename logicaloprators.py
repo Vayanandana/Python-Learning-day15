b3653360cfd3959f691c755874ee2336958e483e
VayanandanaBuddavarapu <vayanandanabud@gmail.com>
2026-01-15 08:53:48 +0530

    Add files via upload

---
+++ logicaloprators.py
@@ -0,0 +1,20 @@
+#The logical operators are used to perform logical operations on Boolean values.
+#Gives True or False as the result. 
+
+#1️ Logical AND (and)
+a = 10
+b = 20
+
+print(a > 5 and b > 15)   # True
+print(a > 15 and b > 15) # False
+
+#2️ Logical OR (or)
+a = 10
+b = 20
+
+print(a > 15 or b > 15)  # True
+print(a < 5 or b < 10)  # False
+
+#3️ Logical NOT (not)
+x = True
+print(not x)   # False
